0b4d47f9915760c8fe4e2b20e3f41fa0c17cb642
omar kazoum <omarKazoum96@gmail.com>
2023-01-02 09:21:17 +0100

    initial commit

---
+++ back-end/src/test/java/com/you/booking/YouBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.you.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

d1e6b3fef1b2a714bee24135cf64b720fecb3d3f
omar kazoum <omarKazoum96@gmail.com>
2023-01-05 23:53:01 +0100

    created register component

--- front-end/src/app/register/register.component.ts
+++ front-end/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
@@ -7,19 +7,36 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['./register.component.scss']
 })
 
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
    FIRST_NAME_KEY:string='first-name';
    LAST_NAME_KEY:string='last-name';
    EMAIL_KEY:string='email';
    PASSWORD_KEY:string='password';
    ROLE_KEY:string='role';
+   roles:string[]=['CLIENT','OWNER'];
 
-  constructor(private authService:AuthenticationService) {
-    this.authService.register({password:'',
+    formData={
+      password:'',
       lastName:'',
       firstName:'',
       email:'',
+      role:'CLIENT'};
+  submitted=false;
+  constructor(private authService:AuthenticationService) {
+    this.authService.register({password:'',
+      lastName:'some last name',
+      firstName:'some first name',
+      email:'some@email.extension',
       role:'CLIENT'});
   }
 
+  ngOnInit(): void {
+      this.formData.email="ezeze";
+    }
+
+  onSubmit() {
+    this.submitted=true;
+    console.log("form was submitted");
+  }
 }
